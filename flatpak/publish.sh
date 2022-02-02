d9bb3117346aa40982451deba2b1f1c306e8f744
Cuber01 <johnmallard631@gmail.com>
2022-02-02 21:50:27 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- flatpak/publish.sh
+++ flatpak/publish.sh
@@ -1,3 +1,4 @@
 mv Moofetch moofetch
 flatpak-builder build-dir org.flatpak.Moofetch.json --force-clean
-
+flatpak build-bundle ~/RiderProjects/Moofetch/flatpak/repo moofetch.flatpak org.flatpak.Moofetch
+echo "Done"
